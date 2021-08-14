4af9c9a19e30f857b286e4459ff2b2d04353242a
JuniimSI <jrfernandesti@gmail.com>
2021-08-14 15:42:15 -0300

    Added the method to destroy contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,7 +8,7 @@ class ContactsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: contacts_param[:record][:email])
+    user = User.find_by(email: contacts_params[:record][:email])
     @contact = current_user.contacts.build(record: user)
     if @contact.save
       flash[:notice] = 'Contact succesfully created'
@@ -18,9 +18,19 @@ class ContactsController < ApplicationController
     end
   end
 
+  def destroy
+    @contact = current_user.contacts.find(params[:id])
+    if @contact.destroy
+      flash[:notice] = "Contact succesfully removed."
+    else
+      flash[:alert] = "An error ocurred when trying to remove this contact"
+    end
+    redirect_to contacts_path
+  end
+
   private
 
-  def contacts_param
+  def contacts_params
     params.require(:contact).permit(record: :email)
   end
 end
